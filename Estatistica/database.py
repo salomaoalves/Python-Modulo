902d7bddb7213b6266aa8bb6909076bf0ba30340
Salomao <salomao@salomao.alves222@gmail.com>
2019-08-24 17:10:24 -0300

    OK

--- Estatistica/database.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-
-'''O modulo sklearn.datasets possui mais datasets'''
-'''o modulo seaborn tem mais dataset, olhar seu repositorio no git'''
-
-#TRABALHANDO COM OS DADOS
-iris = sns.load_dataset('iris') #dados q serao usados
-df = pd.DataFrame(iris) #dados na forma de um DataFrame
--- Estatistica/qui-quadrado.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from scipy.stats import chi2_contingency
-
-novela = np.array([[41,34],[18,7]]) #dados utilizados
-print(chi2_contingency(novela)) #o segundo parametro retornado é o value-p
